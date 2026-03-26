92ab2d731069cca2097e5b1ace276d1e2a958815
HRIS <hris@yourtbc.com>
2026-03-26 15:00:54 +0700

    fix: udpate CI for lint 2

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         </div>
         <button
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          /onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
         </button>
